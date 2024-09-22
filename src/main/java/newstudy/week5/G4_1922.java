d74a2373bf4e26cf736dfe8022639939df99f5cc
Jinwoo <zvzv9808@gmail.com>
2024-09-23 01:59:01 +0900

    study

--- src/main/java/newstudy/week5/G4_1922.java
+++ src/main/java/newstudy/week5/G4_1922.java
@@ -1,2 +1,95 @@
-package newstudy.week5;public class G4_1922 {
+package newstudy.week5;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.StringTokenizer;
+
+public class G4_1922 {
+
+    static class Node implements Comparable<Node>{
+
+        int to;
+        int from;
+        int value;
+
+        public Node(int to, int from, int value) {
+            this.to = to;
+            this.from = from;
+            this.value = value;
+        }
+
+        @Override
+        public int compareTo(Node o) {
+            return this.value - o.value;
+        }
+    }
+
+    static int[] parent;
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        int M = Integer.parseInt(br.readLine());
+
+        ArrayList<Node> list = new ArrayList<>();
+        parent = new int[N + 1];
+
+        for (int i = 0; i <= N; i++) {
+            parent[i] = i;
+        }
+
+        StringTokenizer st;
+
+        for (int i = 0; i < M; i++) {
+
+            st = new StringTokenizer(br.readLine());
+
+            int to = Integer.parseInt(st.nextToken());
+            int from = Integer.parseInt(st.nextToken());
+            int value = Integer.parseInt(st.nextToken());
+
+            list.add(new Node(to, from, value));
+        }
+
+        Collections.sort(list);
+
+        int answer = 0;
+
+        for (int i=0; i<list.size(); i++) {
+
+            Node node = list.get(i);
+
+            int x = find(node.to);
+            int y = find(node.from);
+
+            if (x != y){
+
+                union(node.to, node.from);
+                answer += node.value;
+            }
+        }
+
+        System.out.println(answer);
+    }
+
+    private static int find(int n) {
+
+        if (parent[n] == n) return n;
+
+        return parent[n] = find(parent[n]);
+    }
+
+    private static void union(int x, int y) {
+
+        int dirX = find(x);
+        int dirY = find(y);
+
+        if (dirX != dirY)
+            parent[dirY] = dirX;
+    }
 }
--- src/main/java/newstudy/week5/S1_9465.java
+++ src/main/java/newstudy/week5/S1_9465.java
@@ -1,2 +1,46 @@
-package newstudy.week5;public class S1_9465 {
+package newstudy.week5;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class S1_9465 {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+
+        for (int i=0; i<T; i++) {
+
+            int n = Integer.parseInt(br.readLine());
+
+            int[][] map = new int[2][n + 1];
+            int[][] dp = new int[2][n + 1];
+
+            for (int k=0; k<2; k++) {
+
+                String[] input = br.readLine().split(" ");
+
+                for (int len=1; len<=input.length; len++) {
+
+                    map[k][len] = Integer.parseInt(input[len-1]);
+                }
+            }
+
+            dp[0][1] = map[0][1];
+            dp[1][1] = map[1][1];
+
+            for (int col=2; col<=n; col++) {
+
+                dp[0][col] = Math.max(dp[1][col-2], dp[1][col-1]) + map[0][col];
+                dp[1][col] = Math.max(dp[0][col-2], dp[0][col-1]) + map[1][col];
+            }
+
+            int answer = Math.max(dp[0][n], dp[1][n]);
+
+            System.out.println(answer);
+        }
+    }
 }
