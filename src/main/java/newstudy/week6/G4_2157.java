73479df57aa03506f07aee3c3b1ce9c92e51200a
Jinwoo <zvzv9808@gmail.com>
2024-11-30 23:14:53 +0900

    2157

--- src/main/java/newstudy/week6/G4_2157.java
+++ src/main/java/newstudy/week6/G4_2157.java
@@ -1,2 +1,25 @@
-package newstudy.week6;public class G4_2157 {
+package newstudy.week6;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class G4_2157 {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+
+        int[][] value = new int[N + 1][N + 1];
+
+        for (int i = 0; i < K; i++) {
+
+            
+        }
+    }
 }
